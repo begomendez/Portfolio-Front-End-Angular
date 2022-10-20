3426fbafcfc6500467e1f992a7f2b03f9a7ba12a
begomendez <beguito.ponna87@gmail.com>
2022-10-19 22:39:35 -0300

    portfolioangular

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PieComponent } from './componentes/pie/pie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    InicioComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    PieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
